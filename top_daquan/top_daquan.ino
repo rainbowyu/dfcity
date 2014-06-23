14b5ecafb38ab95a21feedcfcef57cceb91487a5
Roy.yu <roy@jokeai.com>
2014-06-23 18:09:48 +0800

    v0.1

---
+++ top_daquan/top_daquan.ino
@@ -0,0 +1,64 @@
+#include <Wire.h>
+
+#define Address_Bottom 2        //从机地址
+#define DEBUG
+int Distance[3];//存储6个模拟口采集到的数值
+int Dist_Max=0,Num_Max=0;//采集到的六个值中的最大值和最大值所在的号码
+void setup()
+{
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(9600);  // start serial for output
+}
+
+void loop()
+{
+	Num_Max=0;
+	Dist_Max=0;
+	for(int i=0;i<3;i++)
+	{
+		Distance[i]=analogRead(i);//读出第i口的模拟量的值
+	}
+	for(int i=0;i<3;i++)        //找出最大值及其号码
+	{
+		if(Distance[i]>Dist_Max)
+		{
+			Dist_Max=Distance[i];
+			Num_Max=i;
+		}  
+#ifdef DEBUG
+		Serial.print(Distance[i]);
+		Serial.print(" ");
+#endif
+	}
+#ifdef DEBUG
+	Serial.print("Dist_Max:");
+	Serial.println(Dist_Max);
+	Serial.print("Num_Max:");
+	Serial.println(Num_Max);
+#endif
+	 if(Dist_Max>100)                            //根据最大值所在号码发出不同命令
+	 {
+		switch (Num_Max)
+		{
+			case 0:Send_Command('L',150);break;
+			case 1:Send_Command('F',150);break;
+			case 2:Send_Command('R',150);break;
+		}
+	 }
+//	delay(100);//延时100ms
+//	Get_Value(Num_Max);
+}
+
+void Send_Command(char command,int num)
+{
+	Wire.beginTransmission(Address_Bottom); //发送数据给地址为2的器件
+	Wire.write(command);        //发送数据
+	Wire.write(num);        //发送数据
+	Wire.endTransmission();    // 停止发送
+#ifdef DEBUG
+	Serial.print("num:");//串口打印，调试时用到的
+	Serial.println(num);//串口打印，调试时用到的
+#endif
+}
+ 
+
